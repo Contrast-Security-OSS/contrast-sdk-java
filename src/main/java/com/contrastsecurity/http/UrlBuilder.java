da278426d1d55cd0c27c5a6c4cdcdc0616aab6ca
sergeib-gmail <sergeibulatov@gmail.com>
2019-11-04 16:51:46 -0500

    Adding code to get licensed applications only, as opposed to all apps

--- src/main/java/com/contrastsecurity/http/UrlBuilder.java
+++ src/main/java/com/contrastsecurity/http/UrlBuilder.java
@@ -39,6 +39,10 @@ public class UrlBuilder {
         return String.format("/ng/%s/applications?%s", organizationId, "base=false");
     }
 
+    public String getLicensedApplicationsUrl(String organizationId) {
+        return String.format("/ng/%s/applications%s", organizationId, "/filter?expand=license,compliance_policy,skip_links,technologies,trace_severity_breakdown,metadata&sort=appName&quickFilter=LICENSED");
+    }
+
     public String getApplicationsNameUrl(String organizationId) {
         return String.format("/ng/%s/applications/name", organizationId);
     }
