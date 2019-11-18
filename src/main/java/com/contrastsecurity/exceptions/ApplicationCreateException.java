4cc1269a5624c48cabf205349bd41b2a6df2ace2
Bryan Ateo-an <bryan.ateoan@contrastsecurity.com>
2019-11-18 16:37:00 -0500

    Add member variables to hold the response code and response message in ApplicationCreateException

--- src/main/java/com/contrastsecurity/exceptions/ApplicationCreateException.java
+++ src/main/java/com/contrastsecurity/exceptions/ApplicationCreateException.java
@@ -1,8 +1,19 @@
 package com.contrastsecurity.exceptions;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter
 public class ApplicationCreateException extends Exception {
-    public ApplicationCreateException(int rc, String message) {
-        super("Recieved Response code: "+rc+" with message: " + message);
+    private final int rc;
+    private final String responseMessage;
+
+    public ApplicationCreateException(int rc, String responseMessage) {
+        super("Recieved Response code: "+rc+" with message: " + responseMessage);
+        this.rc = rc;
+        this.responseMessage = responseMessage;
     }
+
+    @Getter(AccessLevel.NONE)
     private static final long serialVersionUID = -9049287248312255189L;
 }
