aa8b9a146b0a1d59e9ea073de258765098173498
Brian Phillips <brian.phillips@contrastsecurity.com>
2025-01-16 17:11:22 -0500

    JAVA-8967 update file headers for Lombok

--- sdk/src/test/java/com/contrastsecurity/http/TraceFilterFormTest.java
+++ sdk/src/test/java/com/contrastsecurity/http/TraceFilterFormTest.java
@@ -3,7 +3,7 @@ package com.contrastsecurity.http;
  * #%L
  * Contrast Java SDK
  * %%
- * Copyright (C) 2022 - 2024 Contrast Security, Inc.
+ * Copyright (C) 2022 - 2025 Contrast Security, Inc.
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
